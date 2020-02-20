ea5b14951ee1334b81394cb1fa4e0a6929390513
Katie Duane <katie.duane@ware2go.co>
2020-02-20 13:56:48 -0500

    wip advanced types

---
+++ dist/6_advanced-types/app.js
@@ -0,0 +1,51 @@
+"use strict";
+var e1 = {
+    name: "Katie",
+    privs: ["create-server"],
+    startDate: new Date()
+};
+function addAgainn(a, b) {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInfo(emp) {
+    console.log("Name: ", emp.name);
+    if ("privs" in emp) {
+        console.log("Privileges: ", emp.privs);
+    }
+    if ("startDate" in emp) {
+        console.log("Start date: ", emp.startDate);
+    }
+}
+printEmployeeInfo(e1);
+var Car = (function () {
+    function Car() {
+    }
+    Car.prototype.drive = function () {
+        console.log("driving...");
+    };
+    return Car;
+}());
+var Truck = (function () {
+    function Truck() {
+    }
+    Truck.prototype.drive = function () {
+        console.log("driving a truck...");
+    };
+    Truck.prototype.loadCargo = function (amount) {
+        console.log("loading cargo ", amount);
+    };
+    return Truck;
+}());
+var v1 = new Car();
+var v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(999);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
---
+++ src/6_advanced-types/app.ts
@@ -0,0 +1,80 @@
+// intersection types
+// these allow us to combine other types. it is similar to interface inheritance
+// the intersection operater & can be used with any types, simply building the intersection between them
+type AdminWorker = {
+  name: string;
+  privs: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployee = AdminWorker & Employee;
+
+const e1: ElevatedEmployee = {
+  name: "Katie",
+  privs: ["create-server"],
+  startDate: new Date()
+};
+
+type CombinableToo = string | number;
+type Numeric = number | boolean;
+
+type Universal = CombinableToo & Numeric;
+
+// type guards
+function addAgainn(a: CombinableToo, b: CombinableToo) {
+  // the below line is a 'type guard' to ensure it runs correctly at run time
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | AdminWorker;
+
+function printEmployeeInfo(emp: UnknownEmployee) {
+  console.log("Name: ", emp.name);
+  // below is a way to type guard with custom types that don't exist in JS
+  if ("privs" in emp) {
+    console.log("Privileges: ", emp.privs);
+  }
+  if ("startDate" in emp) {
+    console.log("Start date: ", emp.startDate);
+  }
+}
+
+printEmployeeInfo(e1);
+
+// another way to typeguard with custom types is below, using instanceof instead of typeof
+class Car {
+  drive() {
+    console.log("driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("driving a truck...");
+  }
+  loadCargo(amount: number) {
+    console.log("loading cargo ", amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(999);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
